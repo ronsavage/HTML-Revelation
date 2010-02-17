0924cc6c6320c6f2e7b7b4bccea807ec49e36cb1
Ron Savage <ron@savage.net.au>
2010-02-17 18:49:42 +1100

    Clean up reveal.pl now we're using pod2html.pl

--- scripts/reveal.pl
+++ scripts/reveal.pl
@@ -10,11 +10,11 @@ use HTML::Revelation;
 my($reveal) = HTML::Revelation -> new
 (
  caption          => 1,
- comment          => "DBIx::Admin::CreateTable's POD converted to HTML with fancy-pom2.pl",
- css_output_file  => '/home/ron/httpd/prefork/htdocs/assets/css/local/CreateTable.css',
+ comment          => "DBIx::Admin::CreateTable's POD converted to HTML with my pod2html.pl",
+ css_output_file  => '/home/ron/homepage/Perl-modules/css/CreateTable.css',
  css_url          => '/assets/css/local/CreateTable.css',
- html_output_file => '/home/ron/httpd/prefork/htdocs/CreateTable.html',
- input_file       => '/home/ron/httpd/prefork/htdocs/misc/CreateTable.html',
+ html_output_file => '/home/ron/homepage/Perl-modules/html/CreateTable.html',
+ input_file       => '/home/ron/homepage/Perl-modules/html/DBIx/Admin/CreateTable.html',
 );
 
 $reveal -> run();
