0137ebb0cef53e8cf1392ff443aef126ce678416
ronsavage <ron@savage.net.au>
2011-12-19 16:28:34 +1100

    In scripts/reveal.pl, change hard-coded /home/ron/homepage to savage_net_au_dir

--- scripts/reveal.pl
+++ scripts/reveal.pl
@@ -3,18 +3,21 @@
 use strict;
 use warnings;
 
+use Local::Config;
+
 use HTML::Revelation;
 
 # -----------------------------------------------
 
+my($config) = Local::Config -> new -> config;
 my($reveal) = HTML::Revelation -> new
 (
  caption          => 1,
  comment          => "DBIx::Admin::CreateTable's POD converted to HTML with my pod2html.pl",
- css_output_file  => '/home/ron/homepage/Perl-modules/css/CreateTable.css',
+ css_output_file  => "$$config{savage_net_au_dir}/Perl-modules/css/CreateTable.css",
  css_url          => '/assets/css/local/CreateTable.css',
- html_output_file => '/home/ron/homepage/Perl-modules/html/CreateTable.html',
- input_file       => '/home/ron/homepage/Perl-modules/html/DBIx/Admin/CreateTable.html',
+ html_output_file => "$$config{savage_net_au_dir}/Perl-modules/html/CreateTable.html",
+ input_file       => "$$config{savage_net_au_dir}/Perl-modules/html/DBIx/Admin/CreateTable.html",
 );
 
 $reveal -> run();
